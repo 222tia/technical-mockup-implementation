a1d75b10afc1d705a4ee37186aeaed248e9c3ede
Tia Self <tiamarie.self@bsu.edu>
2025-03-27 21:00:40 -0400

    configure routing for pages

---
+++ src/components/CreateChore.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>create chore page</p>
+</template>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,19 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { createRouter, createWebHistory } from "vue-router";
+import CreateChore from './components/CreateChore.vue';
+import Dashboard from './components/Dashboard.vue';
+import LogIn from './components/LogIn.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", name: "LogIn", component: LogIn },
+    { path: "/Dashboard", name: "Dashboard", component: Dashboard },
+    { path: "/CreateChore", name: "CreateChore", component: CreateChore },
+  ],
+});
+
 createApp(App).mount('#app')
+app.use(router);
